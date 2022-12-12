45fa24d64c78c49b2f638e43f23a292e64b5a84d
Ubuntu <Jenkin@Madhavi.bq3vfz4n1afurlc0nujta4ry5e.rx.internal.cloudapp.net>
2022-12-12 19:14:59 +0000

    Adding a.java to repo

--- a.java
+++ a.java
@@ -2,6 +2,6 @@ public class a
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello! How are you");
+		System.out.println("Hello! Welcome to Jenkins!Demonstrating freestyle job");
 		}
 }
